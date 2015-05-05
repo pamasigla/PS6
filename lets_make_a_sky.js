8108473475a25e22b0e44e913cda27b1a72575b0
Pauline Masigla <pauline.masigla@gmail.com>
2015-05-04 21:34:08 -0400

    update code

--- lets_make_a_sky.js
+++ lets_make_a_sky.js
@@ -17,51 +17,37 @@ animate();
 
 // ## Initialize everything
 function init() {
-	// test if webgl is supported
-	if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
-	// create the camera
-	camera = new THREE.Camera( 70, window.innerWidth / window.innerHeight, 1, 100000 );
+if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+camera = new THREE.Camera( 70, window.innerWidth / window.innerHeight, 1, 100000 );
+scene = new THREE.Scene();
 
-	// create the Scene
-	scene = new THREE.Scene();
-	
-	// ## Begining of the Skybox Code
-	
-	// load the cube textures
-	var urlPrefix	= "images/";
+var urlPrefix	= "images/";
 	var urls = [ urlPrefix + "posx.jpg", urlPrefix + "negx.jpg",
 			urlPrefix + "posy.jpg", urlPrefix + "negy.jpg",
 			urlPrefix + "posz.jpg", urlPrefix + "negz.jpg" ];
 	var textureCube	= THREE.ImageUtils.loadTextureCube( urls );
 	
-	// init the cube shadder
-	var shader	= THREE.ShaderLib["cube"];
-	var uniforms	= THREE.UniformsUtils.clone( shader.uniforms );
-	uniforms['tCube'].texture= textureCube;
-	var material = new THREE.MeshShaderMaterial({
+	var shader = THREE.ShaderLib['cube'];
+	shader.uniforms['tCube'].value= textureCube;
+	var material = new THREE.ShaderMaterial({
 		fragmentShader	: shader.fragmentShader,
 		vertexShader	: shader.vertexShader,
-		uniforms	: uniforms
+		uniforms	: shader.uniforms
 	});
 
-	// build the skybox Mesh
 	skyboxMesh	= new THREE.Mesh( new THREE.CubeGeometry( 100000, 100000, 100000, 1, 1, 1, null, true ), material );
-	// add it to the scene
-	scene.addObject( skyboxMesh );
 
-	// ## End of the Skybox Code
+	scene.add( skyboxMesh );
 
-	// create the container element
 	container = document.createElement( 'div' );
 	document.body.appendChild( container );
 
-	// init the WebGL renderer and append it to the Dom
-	renderer = new THREE.WebGLRenderer();
+renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	container.appendChild( renderer.domElement );
 	
-	// init the Stats and append it to the Dom - performance vuemeter
+
 	stats = new Stats();
 	stats.domElement.style.position = 'absolute';
 	stats.domElement.style.top = '0px';
